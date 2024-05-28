39733f5e3628e0558201384d46dc62a6d8b946be
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2024-05-28 23:57:25 +0700

    initial

--- app/src/main/java/com/example/connect/utils/Constant.kt
+++ app/src/main/java/com/example/connect/utils/Constant.kt
@@ -2,7 +2,7 @@ package com.example.connect.utils
 
 class Constant {
     companion object {
-        const val API_TOKEN = "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"
-        const val BASE_URL = "https://api.github.com/"
+        const val API_TOKEN = ""
+        const val BASE_URL = ""
     }
 }
